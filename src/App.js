1bb0cf60b00ab636946dbd6af591e3b7f0da0038
SimonM14 <kaninisimon79@gmail.com>
2023-09-30 16:20:29 +0300

    edits

--- src/App.js
+++ src/App.js
@@ -3,17 +3,20 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import WelcomePage from "./components/WelcomePage";
 import EntriesPage from "./components/EntriesPage";
 import "./styles.css";
-import { Navbar, Nav, NavbarBrand } from "react-bootstrap";
+import {  Navbar, NavbarBrand } from "react-bootstrap";
 
 const App = () => {
   return (
     <Router>
-      <Navbar bg="light">
-        <NavbarBrand style={{fontSize:23, color: "red"}}>Mi-<t style={{color: "black"}}>Thoughts</t></NavbarBrand>
-        <Nav className="mr-auto">
-          <Link to="/">Home</Link>
+      <Navbar>
+        <NavbarBrand style={{ fontSize: 23, color: "red" }}>
+          Mi-<t style={{ color: "white" }}>Thoughts</t>
+        </NavbarBrand>
+        <nav className="nav">
+          <Link to="/" style={{}}>Home</Link>
           <Link to="/entries">Entries</Link>
-        </Nav>
+          <Link to="/About">About</Link>
+        </nav>
       </Navbar>
       <hr />
       <div className="App">
