648aa0ef00be9c691bb7a00ac3aeb169ec6386e9
SimonM14 <kaninisimon79@gmail.com>
2023-09-30 13:07:16 +0300

    refined app.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import WelcomePage from "./WelcomePage";
-import EntriesPage from "./EntriesPage";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import WelcomePage from "./components/WelcomePage";
+import EntriesPage from "./components/EntriesPage";
 
 const App = () => {
   return (
     <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/entries">Entries</Link>
+          </li>
+        </ul>
+      </nav>
+      <hr />
       <div className="App">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/entries">Entries</Link>
-            </li>
-          </ul>
-        </nav>
-        <hr />
-        <Route exact path="/" component={WelcomePage} />
-        <Route path="/entries" component={EntriesPage} />
+        <Routes>
+          <Route exact path="/" element={<WelcomePage />} />
+          <Route path="/entries" element={<EntriesPage />} />
+        </Routes>
       </div>
     </Router>
   );
 };
 
 export default App;
+
+
+
---
+++ src/components/Navi.js
@@ -0,0 +1,12 @@
+
+<nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/entries">Entries</Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
